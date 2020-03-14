30a2ebde6ffb2ba0b62a68491c4589a23209fae4
ww <qingfenghuohu@126.com>
2020-03-14 21:44:15 +0800

    增加新方法

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 	"os"
 	"os/exec"
 	"reflect"
+	"regexp"
 	"sort"
 	"strconv"
 	"strings"
@@ -260,6 +261,48 @@ func GetFieldName(structName interface{}) []string {
 		result = append(result, t.Field(i).Name)
 	}
 	return result
+}
+
+//check if is mobile broswer
+func IsMobile(r *http.Request) bool {
+	//put headers in a map
+	headers := make(map[string]string)
+
+	//net/http中获取headers
+	if len(r.Header) > 0 {
+		for k, v := range r.Header {
+			headers[k] = v[0]
+			//fmt.Printf("%s=%s\n", k, v[0])
+		}
+	}
+	var is_mobile = false
+	via := strings.ToLower(headers["VIA"])
+	accept := strings.ToUpper(headers["Accept"])
+	HTTP_X_WAP_PROFILE := headers["X_WAP_PROFILE"]
+	HTTP_PROFILE := headers["PROFILE"]
+	HTTP_USER_AGENT := headers["User-Agent"]
+	if via != "" && strings.Index(via, "wap") != -1 {
+		is_mobile = true
+	} else if accept != "" && strings.Index(accept, "VND.WAP.WML") != -1 {
+		is_mobile = true
+	} else if HTTP_X_WAP_PROFILE != "" || HTTP_PROFILE != "" {
+		is_mobile = true
+	} else if HTTP_USER_AGENT != "" {
+
+		reg := regexp.MustCompile(`(?i:(blackberry|configuration\/cldc|hp |hp-|htc |htc_|htc-|iemobile|kindle|midp|mmp|motorola|mobile|nokia|opera mini|opera |Googlebot-Mobile|YahooSeeker\/M1A1-R2D2|android|iphone|ipod|mobi|palm|palmos|pocket|portalmmm|ppc;|smartphone|sonyericsson|sqh|spv|symbian|treo|up.browser|up.link|vodafone|windows ce|xda |xda_|MicroMessenger))`)
+
+		fmt.Printf("%q\n", reg.FindAllString(HTTP_USER_AGENT, -1))
+
+		if len(reg.FindAllString(HTTP_USER_AGENT, -1)) > 0 {
+			is_mobile = true
+		}
+
+	}
+
+	if is_mobile == true {
+		return true
+	}
+	return false
 }
 
 //func main() {
